b8c1d0ee13f70e79b12055f7862c6dceaa0d99a6
thefersh24 <thefersh24@gmail.com>
2025-10-01 18:13:49 +0000

    init

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -21,7 +21,7 @@
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
+        npm-install = "pnpm ci --no-audit --prefer-offline --no-progress --timing";
         # Open editors for the following files by default, if they exist:
         default.openFiles = [
           # Cover all the variations of language, src-dir, router (app/pages)
@@ -30,17 +30,6 @@
           "app/page.tsx" "app/page.js"
           "src/app/page.tsx" "src/app/page.js"
         ];
-      };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
-    };
-    # Enable previews and customize configuration
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
-          manager = "web";
-        };
       };
     };
   };
