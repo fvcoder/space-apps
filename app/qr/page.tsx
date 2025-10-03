3e6458b395b2dd1508dd56dbff6d2037f382eb80
Fernando Ticona <thefersh24@gmail.com>
2025-10-02 21:11:46 -0400

    qr

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -3,12 +3,14 @@ import QRCode from "react-qr-code";
 
 export default function GenQR() {
     return (
-        <div className="bg-white grid grid-cols-4 gap-10 max-w-7xl place-items-center p-10 mx-auto">
-            {Array.from({ length: 10 }).map((_, i) => (
-                <div key={i}>
-                    <QRCode value={Math.random().toString(36).substring(2, 12)} />
-                </div>
-            ))}
+        <div className="bg-white p-4 print:p-0">
+            <div className="grid grid-cols-5 gap-4 print:grid-cols-5">
+                {Array.from({ length: 100 }).map((_, i) => (
+                    <div key={i} className="flex flex-col items-center justify-center" style={{ width: "3cm", height: "3cm" }}>
+                        <QRCode value={Math.random().toString(36).substring(2, 12)} size={113} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
